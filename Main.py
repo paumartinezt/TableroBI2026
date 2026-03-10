aee2934b722125edbff0ed7431bdbb701cdf00d4
paumartinezt <0202549@up.edu.mx>
2026-03-09 21:26:59 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,10 @@
-# Sección de importación de módulos
 from Modulos.UI.header import show_header
-
-# Sección para crear la GUI
-show_header("Mi primera GUI en Streamlit")
-
+import streamlit as st
 from Modulos.data.Ecobiciservice import cargar_estaciones
 from Modulos.UI.mapa import show_mapa_estaciones
 
-df = cargar_estaciones()
+show_header("Mi primera GUI en Streamlit")
+st.write("PRUEBA NUEVA 9:24 PM")
 
+df = cargar_estaciones()
 show_mapa_estaciones(df)
