6772468b90deddd2a4e8afd800a15365d226e06a
paumartinezt <0202549@up.edu.mx>
2026-03-09 21:39:43 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,8 @@
 from Modulos.UI.header import show_header
 from Modulos.data.Ecobiciservice import cargar_estaciones
-from Modulos.UI.mapa import EcobiciViz
+from Modulos.UI.mapa import show_mapa_estaciones
 
 show_header("Mi primera GUI en Streamlit")
 
 df = cargar_estaciones()
-
-viz = EcobiciViz()
-viz.render_map(df)
+show_mapa_estaciones(df)
