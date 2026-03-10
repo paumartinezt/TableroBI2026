3ce18c3958b0d0bb437db065346f93148ee115b6
paumartinezt <0202549@up.edu.mx>
2026-03-09 20:14:24 -0600

    Create header.py

---
+++ Modulos/UI/header.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def show_header(text_title: str):
+    # Layout: logo + title side by side
+    col1, col2 = st.columns([1, 6])
+    
+    with col1:
+        st.image("imagenes/UPlogo.jpg", width=200)
+        
+    with col2:
+        st.title(text_title)
+        st.caption("📘 Developed for: *Business Intelligence (Graduate Level)*")
+        st.caption("Instructor: Edgar Avalos-Gauna (2025), Universidad Panamericana")
